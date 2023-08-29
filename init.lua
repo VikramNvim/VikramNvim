13a50d6a7039b2d563b044f55858b409860cb7c2
not-scripter <xmrinmoy@proton.me>
2023-08-29 18:00:00 +0530

    update

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
-require('opts')      -- Options
-require('vars')
-require('keys')
+require('core.opts')      -- Options
+require('core.vars')
+require('core.keys')
 --LazyNvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
