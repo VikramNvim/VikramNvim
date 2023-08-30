a2c7d874e12df311451ffd196bddc6308a6d20e8
not-scripter <xmrinmoy@proton.me>
2023-08-30 12:59:54 +0530

    update

--- init.lua
+++ init.lua
@@ -2,14 +2,3 @@ require('core.opts')
 require('core.vars')
 require('core.keys')
 require("config.lazy")
-
-
-require('stabline').setup({
-  style = "slant",
-  bg = "#2b2b2b",
-  fg = "#986fec",
-  stab_left   = "",
-  stab_right  = "",
-  inactive_bg = "#181a23",
-  inactive_fg = "#aaaaaa",
-})
