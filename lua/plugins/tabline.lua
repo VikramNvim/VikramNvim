8a1d6d501c7eee5a39837b72f1f9039aaffcd2d7
MRINMOY MONDAL <mm2922894@gmail.com>
2024-02-11 00:33:08 +0530

    update

--- lua/plugins/tabline.lua
+++ lua/plugins/tabline.lua
@@ -3,25 +3,32 @@ return {
   event = 'VeryLazy',
   dependencies = 'nvim-tree/nvim-web-devicons',
   config = function()
-    require('tabby.tabline').use_preset('active_wins_at_tail', {
-  -- theme = {
-  --   fill = 'TabLineFill',
-  --   head = 'TabLine',
-  --   current_tab = 'TabLineSel',
-  --   tab = 'TabLine',
-  --   win = 'TabLine',
-  --   tail = 'TabLine',
-  -- },
-  nerdfont = true,
-  lualine_theme = "catppuccin",
-  tab_name = {
-    name_fallback = function(tabid)
-      return tabid
-    end,
-  },
-  buf_name = {
-    mode = "'unique'|'relative'|'tail'|'shorten'",
-  },
-})
+    vim.api.nvim_set_keymap("n", "<leader>ta", "<cmd>$tabnew<CR>", { noremap = true })
+    vim.api.nvim_set_keymap("n", "<leader>tc", "<cmd>tabclose<CR>", { noremap = true })
+    vim.api.nvim_set_keymap("n", "<leader>to", "<cmd>tabonly<CR>", { noremap = true })
+    vim.api.nvim_set_keymap("n", "<leader>tn", "<cmd>tabn<CR>", { noremap = true })
+    vim.api.nvim_set_keymap("n", "<leader>tp", "<cmd>tabp<CR>", { noremap = true })
+    vim.api.nvim_set_keymap("n", "<leader>tmp", "<cmd>-tabmove<CR>", { noremap = true })
+    vim.api.nvim_set_keymap("n", "<leader>tmn", "<cmd>+tabmove<CR>", { noremap = true })
+    require('tabby.tabline').use_preset('active_wins_at_end', {
+      -- theme = {
+      --   fill = 'TabLineFill',
+      --   head = 'TabLine',
+      --   current_tab = 'TabLineSel',
+      --   tab = 'TabLine',
+      --   win = 'TabLine',
+      --   tail = 'TabLine',
+      -- },
+      nerdfont = true,
+      lualine_theme = "catppuccin",
+      tab_name = {
+        name_fallback = function(tabid)
+          return tabid
+        end,
+      },
+      buf_name = {
+        mode = "'unique'|'relative'|'tail'|'shorten'",
+      },
+    })
   end,
 }
