324abd1cf9dfce49fdece89357da689a70bfb067
MRINMOY MONDAL <mm2922894@gmail.com>
2024-02-11 00:57:55 +0530

    formatter update

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,5 +1,16 @@
 return {
   {
+    "kdheepak/lazygit.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      local keymap = vim.api.nvim_set_keymap
+      local default_opts = { noremap = true, silent = true }
+      keymap("n", "<leader>gg", "<cmd>LazyGit<CR>", default_opts)
+    end
+  },
+  {
     'windwp/nvim-autopairs',
     event = "InsertEnter",
     opts = {},
