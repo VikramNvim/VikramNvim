3a8f51329933dfaa467dabca6166335d18269588
MRINMOY MONDAL <mm2922894@gmail.com>
2024-02-25 01:04:10 +0530

    cmp window update

--- init.lua
+++ init.lua
@@ -2,21 +2,29 @@ require("core.opts")
 require("core.keys")
 require("core.lazy")
 
--- #000000
--- #9399b2
--- #89dceb
--- #f38ba8 
--- #a6e3a1 
--- #f9e2af
--- #89b4fa
--- #f2cdcd
--- #fab387
--- #94e2d5
--- #74c7ec
--- #89dceb
--- #cba6f7
--- #f9e2af
--- #b4befe
--- #f5c2e7
--- #9399b2
--- LightGreen
+-- background:     #000000
+-- foreground:     #CDD6F4
+--  
+-- color0:         #45475A
+-- color8:         #585B70
+--
+-- color1:         #F38BA8
+-- color9:         #F38BA8
+--
+-- color2:         #A6E3A1
+-- color10:        #A6E3A1
+--
+-- color3:         #F9E2AF
+-- color11:        #F9E2AF
+--
+-- color4:         #89B4FA
+-- color12:        #89B4FA
+--
+-- color5:         #F5C2E7
+-- color13:        #F5C2E7
+--
+-- color6:         #94E2D5
+-- color14:        #94E2D5
+--
+-- color7:         #BAC2DE
+-- color15:        #A6ADC8
