8d041b65dd0cf33b2ebf7c8359af746c5d15f18b
MRINMOY MONDAL <mm2922894@gmail.com>
2024-02-25 00:09:08 +0530

    update

--- init.lua
+++ init.lua
@@ -1,5 +1,4 @@
 require("core.opts")
-require("core.vars")
 require("core.keys")
 require("core.lazy")
 
--- lua/plugins/indent.lua
+++ lua/plugins/indent.lua
@@ -2,42 +2,28 @@ return {
   {
     "lukas-reineke/indent-blankline.nvim",
     main = "ibl",
-    -- opts = {
-    config = function()
-    local highlight = {
-      "RainbowFirst",
-      "RainbowSecond",
-  }
-
-  local hooks = require "ibl.hooks"
-  hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-      vim.api.nvim_set_hl(0, "RainbowFirst", { fg = "#f000f0" })
-      vim.api.nvim_set_hl(0, "RainbowSecond", { fg = "#00f0f0" })
-  end)
-
-  require("ibl").setup { indent = { highlight = highlight } }
-      indent = {
-        char = "│",
-        tab_char = "│",
-      },
-      scope = { enabled = false },
-      exclude = {
-        filetypes = {
-          "help",
-          "alpha",
-          "dashboard",
-          "neo-tree",
-          "Trouble",
-          "trouble",
-          "lazy",
-          "mason",
-          "notify",
-          "toggleterm",
-          "lazyterm",
+    opts = {
+        indent = {
+          char = "│",
+          tab_char = "│",
         },
-      }
-    end
-    -- },
+        scope = { enabled = false },
+        exclude = {
+          filetypes = {
+            "help",
+            "alpha",
+            "dashboard",
+            "neo-tree",
+            "Trouble",
+            "trouble",
+            "lazy",
+            "mason",
+            "notify",
+            "toggleterm",
+            "lazyterm",
+          },
+        }
+    },
   },
   {
     "echasnovski/mini.indentscope",
@@ -68,23 +54,4 @@ return {
       })
     end,
   },
-  --Old Config
-  --  "lukas-reineke/indent-blankline.nvim", 
-  --  main = "ibl", 
-  --  config = function()
-  --   local highlight = {
-  --     "RainbowFirst",
-  --     "RainbowSecond",
-  -- }
-  --
-  -- local hooks = require "ibl.hooks"
-  -- hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-  --     vim.api.nvim_set_hl(0, "RainbowFirst", { fg = "#f000f0" })
-  --     vim.api.nvim_set_hl(0, "RainbowSecond", { fg = "#00f0f0" })
-  -- end)
-  --
-  -- require("ibl").setup { indent = { highlight = highlight } }
-
-    require("ibl").setup()
-  --  end
 }
